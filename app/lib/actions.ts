b79183ce9d63468d9727fb586968e0c686dde9e9
MedoevRuslan <mr.r.m3@icloud.com>
2024-02-26 13:30:34 +0200

    add error handling to crud operations

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,44 +17,68 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status')
-  })
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+  try {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status')
+    })
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-
-  await sql`
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date) 
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date}) 
 `
-
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.warn('wrong form data: ', err.issues.join('---'))
+    } else {
+      return {
+        message: 'Database Error: Failed to Create Invoice.' + err,
+      };
+    }
+  }
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  })
+  try {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    })
 
-  const amountInCents = amount * 100;
+    const amountInCents = amount * 100;
 
-  await sql`
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}  
     WHERE id = ${id}
 `
-
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.warn('wrong form data: ', err.issues.join('---'))
+    } else {
+      return {
+        message: 'Database Error: Failed to Edit Invoice.' + err,
+      };
+    }
+  }
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  throw new Error('Failed to Delete Invoice');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (err) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.' + err,
+    };
+  }
   revalidatePath('/dashboard/invoices');
 }
